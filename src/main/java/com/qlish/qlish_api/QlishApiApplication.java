18269038f4ff8ec0f6b4562838cda962bb71d0f4
Kolade-Amire <stephamire@gmail.com>
2024-07-31 18:53:49 +0100

    created java branch

---
+++ src/main/java/com/qlish/qlish_api/QlishApiApplication.java
@@ -0,0 +1,13 @@
+package com.qlish.qlish_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlishApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlishApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qlish/qlish_api/QlishApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qlish.qlish_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlishApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
