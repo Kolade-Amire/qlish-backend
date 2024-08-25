c05e3d46aa361dd248a49071da1cc02d3c3f87a5
Kolade-Amire <stephamire@gmail.com>
2024-08-25 23:58:06 +0100

    token entity added

--- src/main/java/com/qlish/qlish_api/user/Token/TokenEntity.java
+++ src/main/java/com/qlish/qlish_api/user/Token/TokenEntity.java
@@ -1,4 +1,32 @@
 package com.qlish.qlish_api.user.Token;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Data
 public class TokenEntity {
+
+    private Integer id;
+    private String token;
+    private String tokenType;
+    private boolean isExpired;
+    private boolean isRevoked;
+
+    @Override
+    public String toString(){
+        return "User{" +
+                "id=" + id +
+                ", token='" + token + '\'' +
+                ", type='" + tokenType + '\'' +
+                ", isExpired='" + isExpired + '\'' +
+                ", isRevoked='" + isRevoked + '\'' +
+                '}';
+    }
+
 }
