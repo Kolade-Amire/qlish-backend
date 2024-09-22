5cb3c91208eab28d2ffd12d69895ee050ee7c740
Kolade-Amire <stephamire@gmail.com>
2024-09-22 08:26:03 +0100

    question service to get questions for modifications added

--- src/main/java/com/qlish/qlish_api/service/QuestionServiceImpl.java
+++ src/main/java/com/qlish/qlish_api/service/QuestionServiceImpl.java
@@ -1,9 +1,17 @@
 package com.qlish.qlish_api.service;
 
 import com.qlish.qlish_api.dto.EnglishQuestionDto;
+import com.qlish.qlish_api.repository.EnglishQuestionRepository;
+import com.qlish.qlish_api.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class QuestionServiceImpl implements QuestionService {
+    private final EnglishQuestionRepository questionRepository;
+
     @Override
     public Page<EnglishQuestionDto> getAllQuestions() {
         return null;
