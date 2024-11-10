462de038331f05bf2606c5935e6b2bbfb05825b4
Kolade-Amire <stephamire@gmail.com>
2024-11-10 07:24:09 +0100

    update question structure with more subjects

--- src/main/java/com/qlish/qlish_api/enums/math/MathTopic.java
+++ src/main/java/com/qlish/qlish_api/enums/math/MathTopic.java
@@ -1,4 +1,28 @@
 package com.qlish.qlish_api.enums.math;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MathTopic {
+    BASIC_MATH("basic-math"),
+    ALGEBRA("algebra"),
+    GEOMETRY("geometry"),
+    TRIGONOMETRY("trigonometry"),
+    CALCULUS("calculus"),
+    STATISTICS_AND_PROBABILITY("statistics-prob"),
+    LINEAR_ALGEBRA("linear-algebra"),
+    DISCRETE_MATH("discrete-math");
+
+    private final String className;
+
+    public static MathTopic fromClassName(String className) {
+        for (MathTopic item : MathTopic.values()) {
+            if (item.getClassName().equalsIgnoreCase(className)) {
+                return item;
+            }
+        }
+        return null;
+    }
 }
